71777f9d20122c105fd9cdb4ba605d58832f245b
alto <alto.martin@outlook.com>
2023-04-20 09:14:39 +0800

    build: change deploy host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 set :branch, "main"
-server "ubuntu-pd", user: "deploy6", roles: %w{app db web}
+server "ubuntu6", user: "deploy6", roles: %w{app db web}
 
 # role-based syntax
 # ==================
